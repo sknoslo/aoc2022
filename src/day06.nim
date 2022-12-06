d6fbe403da3d2e3cebaa7ac680148ed59a8d44a4
Steffen Olson <sknoslo@gmail.com>
2022-12-06 07:05:47 -0500

    day06

---
+++ src/day06.nim
@@ -0,0 +1,34 @@
+import
+  strutils,
+  std/setutils,
+  aoc2022pkg/benchmark,
+  aoc2022pkg/utils
+
+proc partOne(input: string): string =
+  var curr = 4
+
+  while toSet(input[curr-4..<curr]).len < 4:
+    inc(curr)
+
+  $curr
+
+proc partTwo(input: string): string =
+  var curr = 14
+
+  while toSet(input[curr-14..<curr]).len < 14:
+    inc(curr)
+
+  $curr
+
+when isMainModule:
+  echo "### DAY 00 ###"
+
+  let input = stdin.readInput
+
+  echo "### INPUT ###"
+  echo input
+  echo "### END ###"
+
+  bench(partOne(parsed), partTwo(parsed)):
+    let parsed = input.strip
+
